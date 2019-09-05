366bd3209a46cd8fcb84511a9283eb0bb2da30ff
ajaygurumadu <33026834+ajaygurumadu@users.noreply.github.com>
2019-09-05 22:08:19 +0530

    Add files via upload

---
+++ movie/Data/movieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace movie.Models
+{
+    public class movieContext : DbContext
+    {
+        public movieContext (DbContextOptions<movieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<movie.Models.Movie> Movie { get; set; }
+    }
+}
